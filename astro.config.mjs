f831352e113d57fe45dfe3591ae33b4db8fb6534
franco zuniga <franco.ojeda@est.fi.uncoma.edu.ar>
2025-02-04 00:09:21 -0300

    configuracion de github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     vite: {    plugins: [tailwindcss()],  },
+    site: 'https://francozuniga32.github.io/',
+    base: 'timetable_v1',
 });

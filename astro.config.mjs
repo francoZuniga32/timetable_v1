5716a13de1645137461a5140d6343d1e490fc63e
francoZuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2025-03-17 19:02:33 -0300

    fix: puerto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+const PORT = process.env.PORT || 4000; // Usa el puerto de la variable de entorno o 3000 por defecto
+console.log(PORT);
 
-// https://astro.build/config
 export default defineConfig({
+    site:`http://localhost:${PORT}/`,
+    base: "/",
     vite: {    plugins: [tailwindcss()],  },
     env: {
         schema: {
